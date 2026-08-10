e3a1b2f91933cff818695a8a941a65ef2c98e75d
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-08-10 14:00:16 -0500

    fix(release): configure git_ops

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,24 @@
 import Config
 
+if config_env() == :dev do
+  config :git_ops,
+    mix_project: Jido.VFS.MixProject,
+    changelog_file: "CHANGELOG.md",
+    repository_url: "https://github.com/agentjido/jido_vfs",
+    manage_mix_version?: true,
+    version_tag_prefix: "v",
+    types: [
+      feat: [header: "Features"],
+      fix: [header: "Bug Fixes"],
+      perf: [header: "Performance"],
+      refactor: [header: "Refactoring"],
+      docs: [hidden?: true],
+      test: [hidden?: true],
+      deps: [hidden?: true],
+      chore: [hidden?: true],
+      ci: [hidden?: true]
+    ]
+end
+
 # Import environment specific config
 import_config "#{config_env()}.exs"
